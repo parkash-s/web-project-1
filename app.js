38e6480e5517462fca790b01bb2d5b973d3a5390
parkash-s <webdeveloper.prakash@gmail.com>
2024-09-19 16:15:55 +0530

    server code added

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("first code...");
+// var http = require('http');
+// http.createServer(function(req,res){
+//     res.write("first message from server.");
+//     res.end()
+// }).listen(8080);
+
+const { createServer } = require('node:http');
+// const reload = require('reload');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+//   res.setHeader("Type","corns")
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("mode","same-origin");
+//   res.sendDate("this is for testing..");
+  // res.write('Hello World....');
+  res.end('This text is coming from server..');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
