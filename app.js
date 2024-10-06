d8dc668f55711688f6b9a858bbe7bdc9bb10a673
parkash-s <webdeveloper.prakash@gmail.com>
2024-10-06 20:14:42 +0530

    ch

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const VIEWS_PATH = './client';
+const mustacheExpress = require('mustache-express');
 const path = require('path');
-
+const { default: output } = require('./server');
+// const _header = require('./client/header');
+const port = 5000;
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('client', __dirname + '/public');
+const _header = require('./client/header/');
 
 
 app.get('/', (req, res, next) => {
-    console.log("DWSADSsadsds");
-  });
-express.json()
+    console.log("app is running");
+    // console.log(_header);
+    // res.sendFile(path.join(__dirname, "./hello.txt"));
+    // res.send(path.join(__dirname + server, "index.js"));
+});
 
 app.listen(port, ()=>{
-    console.log(`app is listening at http://localhost:8000`);
+    console.log(`app is listening at http://localhost:${port}`);
 })
