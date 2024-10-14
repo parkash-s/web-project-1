6005d2c964d22fc03ac10b0de71b89dbffd958ca
parkash-s <webdeveloper.prakash@gmail.com>
2024-10-14 07:48:00 +0530

    new pages created according to other contents of website...

---
+++ client/_featuredImage/featuredImageContent.js
@@ -0,0 +1,5 @@
+const featuredImg = `<section class="middle-area pageSize">
+        <section class="featured-image">
+            <img src="img/main-image.jpg" alt="Featured Image">
+        </section>
+</section>`;
